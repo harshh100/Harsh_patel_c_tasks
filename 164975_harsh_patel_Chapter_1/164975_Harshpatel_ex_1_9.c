1fd5b1e7b238e9c037656c4250267d5f37a162cc
Harsh Patel einfochips <Harsh.Patel1@einfochips.com>
2024-07-29 15:33:12 +0530

    folder structure change

---
+++ 164975_harsh_patel_Chapter_1/164975_Harshpatel_ex_1_9.c
@@ -0,0 +1,34 @@
+/*
+*Write a program to copy its input to its output, replacing each string of one or more blanks by a single blank.  
+
+*Author : Harsh Patel (164975)
+
+*Created:17/7/24
+
+*Modified : 26/7/24
+
+*/
+ 
+/* required header files */
+#include <stdio.h>
+
+/* main function */
+int main()
+{
+  int iInput;		 /* input store */
+  int iLastInput = '\0' ; /* last input store */
+  
+  printf("Enter string : ");
+  while ((iInput = getchar()) != EOF) /* input until end of file */
+  {
+    if ( ( iInput != ' ' && iInput != '\t' ))  /* check for single blank */
+    {
+        putchar(iInput);
+    }else if(iLastInput != ' ' && iLastInput != '\t'){
+    	putchar(' ');
+    }
+
+    iLastInput = iInput; /* update last input */
+  }
+  return 0;
+}
